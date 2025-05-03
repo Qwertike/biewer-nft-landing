b05a99627941a8968da5a876101b2ffa8c2fbf1a
Qwertike <qwertike@gmail.com>
2025-05-03 07:52:09 +0200

    Update launch.tsx

--- pages/launch.tsx
+++ pages/launch.tsx
@@ -4,8 +4,7 @@ import {
   ConnectButton,
   useActiveAccount,
   useSendTransaction,
-} from "thirdweb/react";
-
+} from "@thirdweb-dev/react"; // ✅ helyes import
 import {
   createThirdwebClient,
   defineChain,
