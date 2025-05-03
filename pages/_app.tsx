9ca0a981daad8290c9d40252f31139f9d8cc3a0a
Qwertike <qwertike@gmail.com>
2025-05-03 07:44:30 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
+// pages/_app.tsx
 import "../styles/globals.css";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
 import type { AppProps } from "next/app";
+import { ThirdwebProvider } from "thirdweb/react";
+import { createThirdwebClient } from "thirdweb";
+import { polygon } from "thirdweb/chains";
+
+const client = createThirdwebClient({
+  clientId: "4307eea7e413a6850719d8df35c2a217", // a te Thirdweb project kulcsod
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain="polygon">
+    <ThirdwebProvider client={client} chain={polygon}>
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
